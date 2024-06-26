0ab0e49246a402f7c92e4163ca3284c35aa3647f
renee.chen <reedreedchen@gmail.com>
2024-06-26 15:48:26 +0800

    depth

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext('2d');
-draw();
-        
+
+var grassImg=new Image();
+grassImg.src="images/grass.svg";
+
+var pugImg=new Image();
+pugImg.src="images/dog.svg";
+
+var cloudImg=new Image();
+cloudImg.src="images/cloud.svg";
+
+var imageLoaded=0;
+
+Draw();
+
 function onResize()
 {
+    imageLoaded=0;
     context.clearRect(0,0,canvas.height,canvas.width);
-    draw();
+    Draw();
 }
 
 addEventListener("resize",onResize);
 
-function draw()
+
+function Draw()
 {
     // 尺寸
     canvas.width = window.innerWidth;
@@ -21,50 +35,45 @@ function draw()
 
     //草地
     context.fillStyle="#2F9C2F";
-    var _height= canvas.getAttribute("height");
-    var _width=canvas.getAttribute("width");
-    context.fillRect(0,_height*0.8,_width,_height*0.2);
+    context.fillRect(0, canvas.height * 0.8, canvas.width,  canvas.height * 0.2);
 
-    //狗
-    var pugImg=new Image();
-    pugImg.src="images/dog.svg";
-    pugImg.onload = function()
-    {
-        context.drawImage(pugImg, _width*0.2, _height*0.8-pugImg.height, pugImg.width,pugImg.height);
-    }
-
-    //雲
-    var cloudImg=new Image();
-    cloudImg.src="images/cloud.svg";
-    cloudImg.onload= function()
-    {
-        context.drawImage(cloudImg, _width*0.15, _height*0.1, 120,120 * cloudImg.height/cloudImg.width);
-        context.drawImage(cloudImg, _width*0.8, _height*0.2, 90, 90 * cloudImg.height/cloudImg.width);
-    }
-
-    //草
-    var grassImg=new Image();
-    grassImg.src="images/grass.svg";
-    grassImg.onload= function()
-    {
-        context.drawImage(grassImg, _width*0.75, _height*0.8-50, 50,50);
-    }
+    grassImg.onload = onImageLoaded;
+    pugImg.onload = onImageLoaded;
+    cloudImg.onload = onImageLoaded;
     
+    //解鎖
+    SetLoading(false);
+}
+
+function onImageLoaded()
+{
+    imageLoaded++;
+
+    if(imageLoaded===3)
+    {
+        DrawText();
+    }
+}
+
+function DrawText()
+{
+    context.drawImage(cloudImg, canvas.width * 0.15, canvas.height * 0.1, 120,120 * cloudImg.height/cloudImg.width);
+    context.drawImage(cloudImg, canvas.width * 0.8 - 90,  canvas.height * 0.2, 90, 90 * cloudImg.height/cloudImg.width);
+    context.drawImage(grassImg, canvas.width * 0.75-50,  canvas.height * 0.8 - 50, 50, 50);
+    context.drawImage(pugImg, canvas.width * 0.2, canvas.height * 0.84 - pugImg.height, pugImg.width, pugImg.height);
+        
     //字1
     context.font="30px Playwrite US Trad";
     context.textAlign="center";
     context.textBaseLine="middle";
     context.fillStyle="black";
-    context.fillText("I'm Renee",_width*0.5,_height*0.4);
+    context.fillText("I'm Renee", canvas.width *  0.5, canvas.height * 0.4);
 
     //字2
     context.font="20px LXGW WenKai TC";
-    context.fillText("a coder",_width*0.5,_height*0.5);
+    context.fillText("a coder", canvas.width * 0.5, canvas.height * 0.5);
 
-    //解鎖
-    SetLoading(false);
 }
-
 function SetLoading(on)
 {
     document.querySelector("body").setAttribute("style", on? "overflow: hidden" : "overflow: auto");
