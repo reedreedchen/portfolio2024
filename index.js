759853ce96557958f5c6726c5cadc959635afdb0
renee.chen <reedreedchen@gmail.com>
2024-07-31 17:06:06 +0800

    express, ejs, json, gitignore

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-var divs = document.querySelector("body").getElementsByClassName("section");
-onResize();
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import express, { json } from "express";
+import fs from "fs"
 
-function onResize()
-{
-    for(var i=0; i < divs.length; ++i)
+const app = express();
+const port = 3000;
+
+app.use(morgan("tiny"));
+app.use(express.static('public'));
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
+var data;
+fs.readFile(_dirname+"/public/data/data.json","utf8", 
+    (err, _data) => 
     {
-        divs[i].style.minHeight= window.innerHeight+"px";
-    }
-}
+        if (err) throw err;
+        data=JSON.parse(_data);
+    }); 
 
-addEventListener("resize", onResize);
+app.listen(port, ()=>{
+    console.log(`server started on port: ${port}`);
+});
+
+app.get("/", (req, res)=>
+{
+    res.render("index.ejs");
+});
+
+app.get("/content", (req, res)=>
+{
+    res.render("content.ejs", {data: data});
+});
+
+app.get("/contact", (req, res)=>
+{
+    res.render("contact.ejs");
+});
